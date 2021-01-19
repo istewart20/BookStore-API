2409e6154537b628e4ae9391ed7ba35efb1e092a
Iain Stewart <iain.stewart.08@gmail.com>
2021-01-19 23:38:14 +0000

    Remove wwwroot, pages and areas folders

--- BookStore-API/Startup.cs
+++ BookStore-API/Startup.cs
@@ -32,7 +32,7 @@ namespace BookStore_API
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddRazorPages();
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +51,6 @@ namespace BookStore_API
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -60,7 +59,7 @@ namespace BookStore_API
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
